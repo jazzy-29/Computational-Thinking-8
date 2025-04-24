68cfdb724889134329ee2209ee3fce03c864602e
jazzy-29 <jazzyawil@seattleacademy.org>
2025-04-24 18:54:51 +0000

    submit

--- Projects/code_art_intro.py
+++ Projects/code_art_intro.py
@@ -2,16 +2,21 @@
 # ### SETUP ###
 import turtle
 # ###############################################
-
+#directions for turtle
 t = turtle.Turtle()
 t.penup()
-t.goto(-100, -100)
-t.color("green")
+t.goto(0, -100)
+t.color("blue")
 t.pendown()
+#color of backround and pen
+t.color("purple")
+turtle.Screen() .bgcolor("black")
 
-for i in range(3):
-    t.forward(100)
-    t.left(900)
+#repeat directions
+for i in range(100):
+    t.forward(100 + i)
+    t.left(90)
+
 
 
 
