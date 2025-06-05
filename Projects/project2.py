2364bf04e4d22da93b17bb21195644329656f635
jazzy-29 <jazzyawil@seattleacademy.org>
2025-06-05 21:46:18 +0000

    submit

--- Projects/project2.py
+++ Projects/project2.py
@@ -2,8 +2,45 @@
 reliable_points = 0 
 unreliable_points = 0
 
-# question 1
-answer = 
+# Middle
+
+#Question 1
+answer = input("Are you A) good at keeping secrets, or B) not good at keeping secrets?/n")
+if answer == "A" :
+    reliable_points += 1
+elif answer == "B" :
+    unreliable_points += 1
+
+#Question 2
+answer = input("Are you A) able to say no in situations, or B) not able to say no?/n")
+if answer == "A" :
+    reliable_points += 1
+elif answer == "B" :
+    unreliable_points += 1
+
+#Question 3 
+answer = input("Do you  A) give up at the first hurdle, or B) never give up even when somethings hard?/n")
+if answer == "A" :
+    reliable_points += 1
+elif answer == "B" :
+    unreliable_points += 1
+
+#Question 4
+answer = input("Can you A) be honest 24/7, or B) may have my moments where im not completely honest...?/n")
+if answer == "A" :
+    reliable_points += 1
+elif answer == "B" :
+    unreliable_points += 1
+
+
+
+
+
+
+
+
+
+    
   
 
 
